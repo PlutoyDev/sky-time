adf4f1ec05a9fb251b89c26f5ff3f7e86f8dbb63
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-20 23:40:31 +0800

    Basic Headers and Layout

--- packages/client/src/components/DefaultLayout.tsx
+++ packages/client/src/components/DefaultLayout.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head';
-import { ReactNode } from 'react';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import Head from "next/head";
+import { ReactNode } from "react";
+import { ReactQueryDevtools } from "react-query/devtools";
+import AppHeader from "./AppHeader";
 
 type DefaultLayoutProps = { children: ReactNode };
 
@@ -8,13 +9,14 @@ export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
     <>
       <Head>
-        <title>Prisma Starter</title>
+        <title>Sky Time</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* <AppHeader /> */}
       <main>{children}</main>
 
-      {process.env.NODE_ENV !== 'production' && (
+      {process.env.NODE_ENV !== "production" && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
     </>
