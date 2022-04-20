dee22cbffc9dafb19a4acc5a24ed294291ab36e4
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-20 23:41:36 +0800

    fix prisma client

--- packages/client/src/libs/prisma.ts
+++ packages/client/src/libs/prisma.ts
@@ -4,13 +4,15 @@ const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
-const log =
-  process.env.NODE_ENV === "development"
-    ? ["query", "error", "warn"]
-    : ["error"];
-
 export const prisma: PrismaClient =
   prismaGlobal.prisma ||
   new PrismaClient({
-    log,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
+
+if (process.env.NODE_ENV !== "production") {
+  prismaGlobal.prisma = prisma;
+}
