f4fbd0007d04abc220be5ad6d86e147b88d6e57e
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-21 22:09:09 +0800

    Add Next Link to App Header

--- packages/client/src/components/AppHeader.tsx
+++ packages/client/src/components/AppHeader.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { createStyles, Menu, Center, Header, Container, Group, Button, Burger } from '@mantine/core';
+import { createStyles, Menu, Center, Header, Container, Group, UnstyledButton } from '@mantine/core';
 import { useBooleanToggle } from '@mantine/hooks';
 import { ChevronDown } from 'tabler-icons-react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const HEADER_HEIGHT = 60;
 
@@ -11,6 +13,8 @@ const useStyles = createStyles((theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: theme.colors.dark[6],
+    boxShadow: `5px 5px 10px ${theme.colors.dark[9]}`,
   },
 
   links: {
@@ -61,63 +65,66 @@ const links: NavLinks[] = [
     link: '/add',
   },
   {
-    label: 'Configure "Bot"',
+    label: 'Configure Bot',
     link: '/configure',
   },
 ];
 
 interface AppHeaderProps {}
 
+export function NavButton({ label, link, links }: NavLinks) {
+  const router = useRouter();
+  const { classes } = useStyles();
+  const menuItems = links?.map((item) => (
+    <Link href={item.link} passHref>
+      <Menu.Item key={item.label}>{item.label}</Menu.Item>
+    </Link>
+  ));
+  const NavButton = (
+    <UnstyledButton className={classes.link} onClick={() => router.push(link)}>
+      <Center>
+        <span className={classes.linkLabel}>{label}</span>
+        {!menuItems || <ChevronDown size={12} />}
+      </Center>
+    </UnstyledButton>
+  );
+
+  return menuItems ? (
+    <Menu
+      key={label}
+      trigger="hover"
+      delay={0}
+      transition="scale-y"
+      transitionDuration={200}
+      placement="end"
+      gutter={1}
+      control={NavButton}
+      children={menuItems}
+    />
+  ) : (
+    // <NavButton />
+    <Link href={link} passHref children={NavButton} />
+    // NavButton
+  );
+}
+
 export function AppHeader({}: AppHeaderProps) {
   const { classes } = useStyles();
   const [opened, toggleOpened] = useBooleanToggle(false);
 
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => <Menu.Item key={item.link}>{item.label}</Menu.Item>);
-
-    if (menuItems) {
-      return (
-        <Menu
-          key={link.label}
-          trigger="hover"
-          delay={0}
-          transitionDuration={0}
-          placement="end"
-          gutter={1}
-          control={
-            <a href={link.link} className={classes.link} onClick={(event) => event.preventDefault()}>
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <ChevronDown size={12} />
-              </Center>
-            </a>
-          }
-        >
-          {menuItems}
-        </Menu>
-      );
-    }
-
-    return (
-      <a key={link.label} href={link.link} className={classes.link} onClick={(event) => event.preventDefault()}>
-        {link.label}
-      </a>
-    );
-  });
+  const items = links.map((link) => <NavButton key={link.label} {...link} />);
 
   return (
     <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }} mb={120}>
       <Container className={classes.inner} fluid>
         <Group>
-          <Burger opened={opened} onClick={() => toggleOpened()} className={classes.burger} size="sm" />
-          {/* <MantineLogo /> */}
+          {/* <Burger opened={opened} onClick={() => toggleOpened()} className={classes.burger} size="sm" /> */}
+          {/* <AppLogo /> */}
         </Group>
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
-        <Button radius="xl" sx={{ height: 30 }}>
-          Get early access
-        </Button>
+        <Group>{/* <UserButton /> */}</Group>
       </Container>
     </Header>
   );
