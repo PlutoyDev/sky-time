3b6b37fb94b5fdc89b825976563605ef1412b0ec
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-22 23:32:09 +0800

    prettier style changes

--- packages/client/src/pages/api/trpc/[trpc].ts
+++ packages/client/src/pages/api/trpc/[trpc].ts
@@ -1,17 +1,17 @@
 /**
  * This file contains tRPC's HTTP response handler
  */
-import * as trpcNext from "@trpc/server/adapters/next";
-import { createContext } from "~/libs/trpc/server";
-import { appRouter } from "~/router";
+import * as trpcNext from '@trpc/server/adapters/next';
+import { createContext } from '~/libs/trpc/server';
+import { appRouter } from '~/router';
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
   onError({ error }) {
-    if (error.code === "INTERNAL_SERVER_ERROR") {
+    if (error.code === 'INTERNAL_SERVER_ERROR') {
       // send to bug reporting
-      console.error("Something went wrong", error);
+      console.error('Something went wrong', error);
     }
   },
   batching: {
