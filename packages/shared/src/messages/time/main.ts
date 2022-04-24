2d5214f52e2ab44b7715f200e74435ee1b1dcfde
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-24 22:47:22 +0800

    Impl formatter with success

--- packages/shared/src/messages/time/main.ts
+++ packages/shared/src/messages/time/main.ts
@@ -10,5 +10,22 @@ export function formatter<
   type TArgs = keyof TStrings | [keyof TEnables, string] | [keyof TFormats, number];
 
   return (str: TemplateStringsArray, ...args: TArgs[]) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      const arg = args[i];
+      if (typeof arg === 'string') {
+        result += str[i] + strings[arg];
+      } else if (Array.isArray(arg)) {
+        const [key, value] = arg;
+        if (typeof key === 'string') {
+          if (typeof value === 'string') {
+            result += str[i] + (enables[key] ? value : '');
+          } else if (typeof value === 'number') {
+            result += str[i] + timeFormatter(formats[key], value);
+          }
+        }
+      }
+    }
+    return result;
   };
 }
