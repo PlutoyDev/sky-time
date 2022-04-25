39d88fb9fbe9d1532dd3b5d1bddbe8792d045400
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-04-25 16:34:54 +0800

    swtich formatter with a simpler formatter

--- packages/shared/src/messages/time/main.ts
+++ packages/shared/src/messages/time/main.ts
@@ -1,3 +1,33 @@
+import { add, addDays, differenceInDays, nextSunday, startOfDay, sub, getUnixTime, fromUnixTime } from 'date-fns';
+import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
+import { ITimeConfig } from '../../model/TimeConfig';
+
+const tsPivotDate = new Date(2022, 0, 10);
+const tsPivotNum = 52;
+
+const mainStrings = [
+  ['title_main', '⏰ **__Main game Timestamps__**'],
+  ['label_last_update', 'Last Updated:'],
+  ['label_daily_reset', 'Daily Reset: '],
+  ['label_eden_reset', 'Eden Reset: '],
+  ['title_traveling_spirit', '__Traveling Spirit__'],
+  ['label_traveling_spirit_arrival', 'Arrival: '],
+  ['label_traveling_spirit_departure', 'Departure: '],
+] as const;
+
+const mainEnables = [
+  ['show_last_update', true],
+  ['show_traveling_spirit_number', true],
+  ['show_promotion', true],
+] as const;
+
+const mainFormats = [
+  ['last_update', '%t (%R)'],
+  ['daily_reset', '%f (%R)'],
+  ['eden_reset', '%f (%R)'],
+  ['traveling_spirit_arrival', '%f (%R)'],
+  ['traveling_spirit_departure', '%f (%R)'],
+] as const;
 export function timeFormatter(format: string, unix: number) {
   return format.replaceAll(/%([tTdDfFR]?)/g, (match, f) => `<t:${unix}` + (f ? `:${f}>` : '>'));
 }
@@ -7,7 +37,14 @@ export function formatter<
   TEnables extends Record<string, boolean>,
   TFormats extends Record<string, string>,
 >(strings: TStrings, enables: TEnables, formats: TFormats) {
-  type TArgs = keyof TStrings | [keyof TEnables, string] | [keyof TFormats, number];
+  type TArgs =
+    | keyof TStrings
+    | [keyof TFormats, number]
+    | [keyof TEnables, keyof TStrings | [keyof TFormats, number] | string];
+
+  const kStrings = Object.keys(strings) as Array<keyof TStrings>;
+  const kEnables = Object.keys(enables) as Array<keyof TEnables>;
+  const kFormats = Object.keys(formats) as Array<keyof TFormats>;
 
   return (str: TemplateStringsArray, ...args: TArgs[]) => {
     let result = '';
@@ -18,14 +55,77 @@ export function formatter<
       } else if (Array.isArray(arg)) {
         const [key, value] = arg;
         if (typeof key === 'string') {
-          if (typeof value === 'string') {
-            result += str[i] + (enables[key] ? value : '');
-          } else if (typeof value === 'number') {
-            result += str[i] + timeFormatter(formats[key], value);
+          if (kFormats.includes(key)) {
+            result += str[i] + timeFormatter(formats[key], value as number);
+          }
+          if (kEnables.includes(key)) {
+            if (enables[key]) {
+              if (typeof value === 'string') {
+                if (kStrings.includes(value)) {
+                  result += str[i] + strings[value];
+                } else {
+                  result += str[i] + value;
+                }
+              } else if (Array.isArray(value) && value) {
+                const [key2, value2] = value;
+                if (typeof key2 === 'string') {
+                  if (kFormats.includes(key2)) {
+                    result += str[i] + timeFormatter(formats[key2], value2);
+                  }
+                }
+              }
+            }
           }
         }
       }
     }
     return result;
+  };
+}
+
+export function generateMessage<TUnixes extends Record<string, number>, TStrings extends Record<string, string>>(
+  timestamps: TUnixes,
+  strings: TStrings,
+  formats: Record<keyof TUnixes, string>,
+) {
+  const kUnixes = Object.keys(timestamps) as Array<keyof TUnixes>;
+  const kStrings = Object.keys(strings) as Array<keyof TStrings>;
+
+  type TArgs = keyof TStrings | keyof TUnixes;
+
+  return (str: TemplateStringsArray, ...args: TArgs[]) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      const arg = args[i];
+      if (typeof arg !== 'string') {
+        continue;
+      } else if (kStrings.includes(arg)) {
+        result += str[i] + strings[arg];
+      } else if (kUnixes.includes(arg)) {
+        result += str[i] + timeFormatter(formats[arg], timestamps[arg]);
+      }
+    }
+    return result;
+  };
+}
+
+export function skyToUnix(date: Date) {
+  return getUnixTime(zonedTimeToUtc(date, 'America/Los_Angeles'));
+}
+
+export function calculateUnixes(currentTime: number): Record<typeof mainFormats[number][0], number> {
+  const now = utcToZonedTime(fromUnixTime(currentTime), 'America/Los_Angeles');
+  const today = startOfDay(now);
+  const daily_reset = add(today, { days: 1 });
+  const eden_reset = nextSunday(today);
+  const traveling_spirit_departure = addDays(today, 14 - (differenceInDays(today, tsPivotDate) % 14));
+  const traveling_spirit_arrival = sub(traveling_spirit_departure, { days: 4 });
+
+  return {
+    last_update: currentTime,
+    daily_reset: skyToUnix(daily_reset),
+    eden_reset: skyToUnix(eden_reset),
+    traveling_spirit_arrival: skyToUnix(traveling_spirit_arrival),
+    traveling_spirit_departure: skyToUnix(traveling_spirit_departure),
   };
 }
--- packages/shared/src/test/messages/time/main.test.ts
+++ packages/shared/src/test/messages/time/main.test.ts
@@ -1,4 +1,4 @@
-import { timeFormatter, formatter } from '../../../messages/time/main';
+import { timeFormatter, MainConfig, main, calculateUnixes } from '../../../messages/time/main';
 
 describe('timeFormatter', () => {
   const unixTime = 1598486400;
@@ -19,37 +19,80 @@ describe('timeFormatter', () => {
   });
 });
 
-describe('formatter', () => {
-  const strings = {
-    title: '**__This is a title__**',
-    label: 'this is a label',
-  };
-  const enables = {
-    falsy: false,
-    truthy: true,
-  };
-  const formats = {
-    now: '%',
-    ytd: '%D',
-    justNow: '%R',
-  };
+// describe('formatter', () => {
+//   const strings = {
+//     title: '**__This is a title__**',
+//     label: 'this is a label',
+//   };
+//   const enables = {
+//     falsy: false,
+//     truthy: true,
+//   };
+//   const formats = {
+//     now: '%',
+//     ytd: '%D',
+//     justNow: '%R',
+//   };
 
-  const testF = formatter(strings, enables, formats);
-  const timeRef = 1598486400;
+//   const testF = formatter(strings, enables, formats);
+//   const timeRef = 1598486400;
 
-  it('should be able to parse strings', () => {
-    expect(testF`${'title'}`).toBe(strings.title);
-    expect(testF`${'label'}`).toBe(strings.label);
-  });
+//   it('should be able to parse strings', () => {
+//     expect(testF`${'title'}`).toBe(strings.title);
+//     expect(testF`${'label'}`).toBe(strings.label);
+//   });
 
-  it('should be able to parse enables', () => {
-    expect(testF`${['falsy', 'Oops']}`).toBe('');
-    expect(testF`${['truthy', 'Good']}`).toBe('Good');
-  });
+//   it('should be able to parse enables', () => {
+//     expect(testF`${['falsy', 'Oops']}`).toBe('');
+//     expect(testF`${['truthy', 'Good']}`).toBe('Good');
+//     expect(testF`${['falsy', 'label']}`).toBe('');
+//     expect(testF`${['truthy', 'label']}`).toBe('this is a label');
+//   });
 
-  it('should be able to parse formats', () => {
-    expect(testF`${['now', timeRef]}`).toBe(`<t:${timeRef}>`);
-    expect(testF`${['ytd', timeRef - 24 * 60 * 60]}`).toBe(`<t:${timeRef - 24 * 60 * 60}:D>`);
-    expect(testF`${['justNow', timeRef - 60 * 60]}`).toBe(`<t:${timeRef - 60 * 60}:R>`);
+//   it('should be able to parse formats', () => {
+//     expect(testF`${['now', timeRef]}`).toBe(`<t:${timeRef}>`);
+//     expect(testF`${['ytd', timeRef - 24 * 60 * 60]}`).toBe(`<t:${timeRef - 24 * 60 * 60}:D>`);
+//     expect(testF`${['justNow', timeRef - 60 * 60]}`).toBe(`<t:${timeRef - 60 * 60}:R>`);
+//   });
+// });
+
+// describe('Main', () => {
+//   const expected = `⏰ **__Main game Timestamps__**
+// Last Updated: <t:1650850349:t> (<t:1650850349:R>)
+
+// Daily Reset: <t:1650870000> (<t:1650870000:R>)
+// Eden Reset: <t:1651388400> (<t:1651388400:R>)
+
+// __Traveling Spirit 60__
+// Arrival: <t:1651129200> (<t:1651129200:R>)
+// Departure: <t:1651474740> (<t:1651474740:R>)`;
+
+//   const config: MainConfig = {
+//     strings: {},
+//     enables: {},
+//     formats: {},
+//   };
+
+//   const generated = main(config, 1650850349);
+//   const eLines = expected.split('\n');
+//   const gLines = generated.split('\n');
+
+//   it('should generate the correct number of lines', () => {
+//     expect(gLines.length).toBe(eLines.length);
+//   });
+
+//   test.each(gLines.map((line, i) => [i, line, eLines[i]]))('Line %d', (i, g, e) => {
+//     expect(g).toBe(e);
+//   });
+// });
+
+describe('calculateUnixes', () => {
+  it('should be able to calculate from 1650850349', () => {
+    const unixes = calculateUnixes(1650850349);
+    expect(unixes.last_update).toBe(1650850349);
+    expect(unixes.daily_reset).toBe(1650870000);
+    expect(unixes.eden_reset).toBe(1651388400);
+    expect(unixes.traveling_spirit_arrival).toBe(1651129200);
+    expect(unixes.traveling_spirit_departure).toBe(1651474800);
   });
 });
