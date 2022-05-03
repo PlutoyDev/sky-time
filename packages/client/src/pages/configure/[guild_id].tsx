3a80788b227964b2fc668fd230f5ddb46ade6a2f
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-05-03 23:28:15 +0800

    delete old authentication UI

--- packages/client/src/pages/configure/[guild_id].tsx
+++
@@ -1,8 +0,0 @@
-import Custom404 from '../404';
-import { Center, Paper, Text } from '@mantine/core';
-
-function ConfigureGuildPage() {
-  return Custom404();
-}
-
-export default ConfigureGuildPage;
